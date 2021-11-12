f7e5f3c60cb35f2e5d473e5c2ea7a5d9617643dd
qcyblm <qcyblm@139.com>
2021-11-12 21:42:30 +0800

    add friendLink

--- js/links.js
+++ js/links.js
@@ -1,114 +1,109 @@
 export default {
   title: "友情链接",
-  children: [{
-      text: "大贤者初春令月",
-      title: "致力于编写使用最少的语法特性和第三方库的紧凑实现",
-      link: "https://mourinaruto.github.io/"
+  children: [
+    {
+      text: '大贤者初春令月',
+      link: 'https://mourinaruto.github.io/',
+      icon: 'https://mourinaruto.github.io/zh/img/favicon.png',
     },
     {
-      text: "Dism++",
-      title: "全球第一款基于 CBS 的 Dism GUI 实现。",
-      link: "https://www.chuyu.me/"
+      text: 'Dism++',
+      link: 'https://www.chuyu.me/',
+      icon: 'https://www.chuyu.me/images/favicon.ico'
     },
     {
-      text: "WINOS",
-      link: "https://www.winos.me/",
-      title: "前沿的精简系统下载"
+      text: 'WINOS',
+      link: 'https://www.winos.me/',
+      icon: 'https://www.winos.me/favicon.ico'
     },
     {
-      text: "提莫酱的博客",
-      link: "https://www.timochan.cn/",
-      title: "Let's start learning !"
+      text: '提莫酱的博客',
+      link: 'https://www.timochan.cn/',
+      icon: 'https://sdn.geekzu.org/avatar/c706e7077f01c4518064fa37c7818c1d&quot'
     },
     {
-      text: "智慧问题",
-      link: "https://blog.zhissue.cn/",
-      title: "Hello,world！I will solve your issue"
+      text: '智慧问题',
+      link: 'https://blog.zhissue.cn/',
     },
     {
-      text: "阿猫阿狗",
-      link: "https://blog.yhz610.com/",
-      title: "我一直都在流浪，可我不曾见过海洋"
+      text: '阿猫阿狗',
+      link: 'https://blog.yhz610.com/',
+      icon: 'https://blog.yhz610.com/img/favicon.ico'
     },
     {
-      text: "Tien's blog",
-      link: "https://www.dodolo.top/",
-      title: "The truth is what it is, not what you see."
+      text: 'Tien\'s blog',
+      link: 'https://www.dodolo.top/',
+      icon: 'https://www.dodolo.top/assets/img/avatar.jpg'
     },
     {
-      text: "龙爪槐守望者",
-      link: "https://www.ftium4.com/",
-      title: "一个帮助交互设计师成长的互联网传说"
+      text: '龙爪槐守望者',
+      link: 'https://www.ftium4.com/',
+      icon: 'https://pic.ftium4.com/20201226025044.png'
     },
     {
-      text: "ql's HexoBlog",
-      link: "https://ql-isaac.cn/",
-      title: "It’s a beautiful day!"
+      text: 'ql\'s',
+      link: 'https://ql-isaac.cn/',
+      icon: 'https://image.ql-isaac.cn/%E7%AB%99%E9%95%BF%E5%A4%B4%E5%83%8F.jpg'
     },
     {
-      text: "爱即是诗",
-      link: "https://www.aigisss.com/blog/",
-      title: "AI&GIS博客"
+      text: '爱即是诗',
+      link: 'https://www.aigisss.com/blog/',
+      icon: 'https://aigisss.com/media/icons/favicon.png'
     },
     {
-      text: "御驾亲征",
-      link: "https://evrstr.cc/",
-      title: "好好学习，天天向上！"
+      text: '御驾亲征',
+      link: 'https://evrstr.cc/',
+      icon: 'https://cdn.jsdelivr.net/gh/evrstr/img/blog_evrstr/avatar.png'
     },
     {
-      text: "来日方长",
-      link: "https://www.kococ.cn/",
-      title: "将我收藏，来日方长"
+      text: '北青永恒',
+      link: 'https://www.kococ.cn/',
+      icon: 'https://www.kococ.cn/favicon.ico'
     },
     {
-      text: "1980's Maker",
-      link: "https://80shuo.com/",
-      title: "一个出生于80年代的程序员"
+      text: '1980\'s Maker',
+      link: 'https://80shuo.com/',
+      icon: 'https://80shuo.com/logo.jpg'
     },
     {
-      text: "Masle’Blog",
-      link: "https://masle.gitee.io/",
-      title: "静以制动，明以破军"
+      text: 'XUNX',
+      link: 'https://blog.xuzhipeng.com/',
+      icon: 'https://xuzhipeng.com/images/X.png'
     },
     {
-      text: "斯人若彩虹",
-      link: "https://bruce-jj.gitee.io/blog/",
-      title: "等风也等你"
+      text: 'Masle’Blog',
+      link: 'https://masle.gitee.io/',
+      icon: 'https://masle.gitee.io/favicon.png'
     },
     {
-      text: "漉鲸的Blog",
-      link: "https://blog.lj0.top/",
-      title: "个人向，记录生活的博客。"
+      text: '斯人若彩虹',
+      link: 'https://bruce-jj.gitee.io/blog/',
+      icon: 'https://bruce-jj.gitee.io/blog/images/avatar.jpg'
     },
     {
-      text: "泰戈尔のBlog",
-      link: "https://www.iftiger.com/",
-      title: "智慧成就科技，科技改变生活。"
+      text: '漉鲸的Blog',
+      link: 'https://blog.lujing0.com/',
+      icon: 'https://blog.lujing0.com/favicon.ico'
     },
     {
-      text: "二丫讲梵",
-      link: "https://wiki.eryajf.net/",
-      title: "💻学习📝记录🔗分享"
+      text: '泰戈尔のBlog',
+      link: 'https://www.iftiger.com/',
+      icon: 'https://cdn.jsdelivr.net/gh/IT-Tagore/images/data/logoico.svg'
     },
     {
-      text: "Legna",
-      link: "https://www.brighost.com/",
-      title: "让一切更简单"
+      text: '二丫讲梵',
+      link: 'https://wiki.eryajf.net/',
+      icon: 'https://wiki.eryajf.net/img/favicon.ico'
     },
     {
-      text: "极客街社区",
-      link: "https://geekjie.com",
-      title: "开放的技术交流社区"
+      text: 'Legna',
+      link: 'https://www.brighost.com/',
+      icon: 'https://www.brighost.com/wp-content/uploads/cropped-2018081501165888-32x32.png'
     },
     {
-      text: "⋛⊱ʚ前端小行星ɞ⊰⋚",
-      link: "http://www.hph.fan/",
-      title: ""
-    },
-    {
-      text: "XUNX",
-      link: "https://blog.xuzhipeng.com/",
-      title: ""
+      text: '阿超',
+      link: 'https://vampireachao.gitee.io/',
+      icon: 'https://waibi.oss-cn-chengdu.aliyuncs.com/2020-06-01/head.jpg'
     }
   ]
 }
